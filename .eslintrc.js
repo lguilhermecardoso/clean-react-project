dc7dd4bd3ef1b1f264516721168c52392e605cf6
guilherme <guilhermecardoso.info@gmail.com>
2020-05-05 19:24:28 -0300

    add suport react-Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,24 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["react", "prettier"],
+  plugins: [
+    "react",
+    "prettier",
+    "react-hooks"
+  ],
   rules: {
     "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "import/prefer-default-export": "off"
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"] 
+      }
+    ],
+    "import/prefer-default-export": "off",
+    'no-param-reassing': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };
